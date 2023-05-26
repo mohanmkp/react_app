e7ec44fbc6c083db3fc3a3cc5bc434ab03b4ba53
Mohan Pandit <mohanpandit131@gmail.com>
2023-05-26 15:21:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/home";
+import Navbar from "./components/navbar";
+
+
+function App() {
+
+return <BrowserRouter>
+<Navbar/>
+  <Routes>
+    <Route path="/" element={ <Home/> } />
+  </Routes>
+</BrowserRouter>
+}
+
+export default App;
+
